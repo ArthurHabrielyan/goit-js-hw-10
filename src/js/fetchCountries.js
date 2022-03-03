b71556cdcd38962dc5aa3713a34a962699dc5a16
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-03-03 15:18:51 +0200

    10 hw ready to check

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(countryName) {
+  return fetch(
+    `${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`,
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      return response.json();
+    }
+  });
+}
+
+export default { fetchCountries };
---
+++ src/js/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    searchForm: document.querySelector('#search-box'),
+    countriesList: document.querySelector('.country-list'),
+    countryContainer: document.querySelector('.country-info'),
+  };
+}
