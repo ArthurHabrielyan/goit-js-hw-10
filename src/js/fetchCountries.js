d2bf0b41ee961708ed4e4ffbec8b0e22d44da643
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-03-09 18:47:17 +0200

    add catch for fetch response

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,15 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(countryName) {
-  return fetch(
-    `${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`,
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    } else {
-      return response.json();
-    }
-  });
+  return fetch(`${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      } else {
+        return response.json();
+      }
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
 
 export default { fetchCountries };
